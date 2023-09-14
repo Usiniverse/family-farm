135d327ffe21125ba30084d9e29df34c78093321
강유신 <rkddbtls007@naver.com>
2023-09-15 02:32:41 +0900

    네이버 로그인 토큰 확인 성공

--- index.ts
+++ index.ts
@@ -6,6 +6,22 @@ import { userRouter } from './src/users/router'
 import cookieSession from 'cookie-session'
 import passport from 'passport'
 import session from 'express-session';
+import { userRepo } from './src/users/index'
+import jwt from 'jsonwebtoken'
+const { Strategy: NaverStrategy, Profile: NaverProfile } = require('passport-naver-v2');
+import dotenv from 'dotenv'
+import { naverCallback } from './src/users/naverCallback'
+import { log } from 'console';
+dotenv.config()
+
+interface IProfile {
+   id: string;
+   response: {
+     email: string;
+     name: string;
+     profile_image: string;
+   };
+ }
 
 const appServer = async () => {
     const app = express();
@@ -30,6 +46,63 @@ const appServer = async () => {
     app.use(passport.session());
     app.use(session({ secret: 'your-secret-key', resave: false, saveUninitialized: false }));
 
+    // ---------------------패스포트 설정 ---------------------------
+    passport.use(
+        // 'naver',
+        new NaverStrategy(
+           {
+              clientID: process.env.NAVER_ID,
+              clientSecret: process.env.NAVER_SECRET,
+              callbackURL: '/users/naver/callback',
+           },
+           async (accessToken: any, refreshToken: any, profile: IProfile, done: any) => {
+              console.log("accessToken ::: ", accessToken);
+              console.log("refreshToken ::: ", refreshToken);
+              console.log("네이버 회원정보::: ", profile)
+  
+              // return done({ accessToken, refreshToken })
+              
+              try {
+                 const exUser = await userRepo.getUser(profile.response.email);
+                 // 이미 가입된 네이버 프로필이면 성공
+                 if (exUser) {
+                    const accessToken = jwt.sign({ id: exUser.id }, 'jwt-secret-key', {
+                       algorithm: 'HS256',
+                       expiresIn: '1d'
+                    }) 
+  
+                    const refreshToken = jwt.sign({ id: exUser.id }, 'jwt-secret-key', {
+                       algorithm: 'HS256',
+                       expiresIn: '14d'
+                    })
+  
+                    const user = {
+                       email: profile.response.email,
+                       nickname: profile.response.name,
+                       snsId: profile.id,
+                       ProfileImages: profile.response.profile_image,
+                       accessToken: accessToken,
+                       refreshToken: refreshToken,
+                       provider: 'naver'
+                     };
+  
+                    done(null, user);
+                 } 
+              } catch (error) {
+                 console.error(error);
+                 done(error);
+              }
+           },
+        ),
+    );
+
+    passport.serializeUser(function (user: any, done) {
+        done(null, user)
+      });
+
+    passport.deserializeUser(function (user: any, done) {
+    done(null, user);
+    });
 
     app.get('/', (req: Request, res: Response, next: NextFunction) => {
         res.send('Hello World!');
@@ -43,7 +116,13 @@ const appServer = async () => {
         `);
     })
 
-    app.use('/users', userRouter)
+    // 네이버 로그인
+    app.get('/users/naver', passport.authenticate('naver'));
+
+    // 네이버 로그인 콜백
+    app.get("/users/naver/callback", passport.authenticate('naver'), naverCallback)
+
+    // app.use('/users', userRouter)
     // app.post("/users", createUserController.createUserController)
 }
 
