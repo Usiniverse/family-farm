c04bbaef6cf5bb2546ded286b2934b26722ef11d
강유신 <rkddbtls007@naver.com>
2023-09-19 21:21:23 +0900

    feat: 카카오 로그인 제거

--- src/users/index.ts
+++ src/users/index.ts
@@ -1,6 +1,3 @@
-// import CreateUserByKakaoController from "./createUserController";
-// import KakaoLoginService from "../../shared/Oauth/kakaoService";
-// import {CreateUserByKaKaoUseCase} from "./createUserByKaKaoUseCase";
 import { UserService } from "./userService"
 import { UserController } from "./userController";
 import { KnexUserRepo } from "./userRepository";
@@ -10,12 +7,6 @@ import { applefarmDB } from "../../shared/lib/db";
 const userRepo = new KnexUserRepo(applefarmDB);
 const createUserService = new UserService(userRepo)
 const createUserController = new UserController(createUserService)
-
-// 카카오 로그인
-// const kakaoLoginService = new KakaoLoginService();
-// const createUserByKakaoUseCase = new CreateUserByKaKaoUseCase(createUserRepo);
-// const createUserUseCase = new CreateUserUseCase(createUserRepo);
-// const createUserByKaKaoController = new CreateUserByKakaoController(kakaoLoginService, createUserByKakaoUseCase, createUserUseCase)
 
 export { createUserService, createUserController }
 
