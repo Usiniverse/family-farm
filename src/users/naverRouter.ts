99c37cfc4250d9c5e205a998e4a17139935e5f23
강유신 <rkddbtls007@naver.com>
2023-09-21 01:23:38 +0900

    feat: 네이버 회원 정보 조회 성공

--- src/users/naverRouter.ts
+++ src/users/naverRouter.ts
@@ -5,6 +5,7 @@ import { Request, Response, NextFunction } from 'express'
 import dotenv from 'dotenv'
 dotenv.config()
 const fetch = require('node-fetch')
+const request = require('request-promise')
 
 const { isLoggedIn, isNotLoggedIn, isUser } = require('../../shared/middleware/authMiddleware')
 
@@ -27,67 +28,109 @@ export interface IProfile {
 authRouter.get('/naver', isNotLoggedIn ,passport.authenticate('naver', { authType: 'reprompt' }));
 
 // 네이버 로그인 콜백
-authRouter.get("/naver/callback", isNotLoggedIn, async (req: Request, res: Response, next: NextFunction) => {
-   passport.authenticate(
-         "naver",
-         { failureRedirect: '/' },
-         async (err: any, user: any, info: any) => {
-            if (!user) {
-               return res.status(401).send("Authentication failed");
-            }
+// authRouter.get("/naver/callback", isNotLoggedIn, async (req: Request, res: Response, next: NextFunction) => {
+//    passport.authenticate(
+//          "naver",
+//          { failureRedirect: '/' },
+//          async (err: any, user: any, info: any) => {
+//             if (!user) {
+//                return res.status(401).send("Authentication failed");
+//             }
 
-            if (err) return next(err)
+//             if (err) return next(err)
 
-            try {
-               const loginError = await new Promise<void>((resolve, reject) => {
-                  req.login(user, (err) => {
-                     if (err) {
-                        console.error(err);
-                        reject(err);
-                     } else {
-                        resolve();
-                     }
-                  });
-               });
+//             try {
+//                const loginError = await new Promise<void>((resolve, reject) => {
+//                   req.login(user, (err) => {
+//                      if (err) {
+//                         console.error(err);
+//                         reject(err);
+//                      } else {
+//                         resolve();
+//                      }
+//                   });
+//                });
 
-               console.log('user:::', user);
+//                console.log('user:::', user);
             
-               const { snsId, email, name, accessToken } = user;
-               const result = {
-                  accessToken,
-                  snsId,
-                  email,
-                  name
-               }
+//                const { snsId, email, name, accessToken } = user;
+//                const result = {
+//                   accessToken,
+//                   snsId,
+//                   email,
+//                   name
+//                }
 
-               res.send({ result })
+//                res.send({ result })
 
-               // if (result.accessToken) {
-               //    const header = "Bearer " + accessToken;
-               //    console.log('token :::', header);
+//                // if (result.accessToken) {
+//                //    const header = "Bearer " + accessToken;
+//                //    console.log('token :::', header);
                   
-               //    const userDataRaw = await fetch("https://openapi.naver.com/v1/nid/me", {
-               //       method: "GET",
-               //       headers: {
-               //          Authorization: header
-               //       }
-               //    });
+//                //    const userDataRaw = await fetch("https://openapi.naver.com/v1/nid/me", {
+//                //       method: "GET",
+//                //       headers: {
+//                //          Authorization: header
+//                //       }
+//                //    });
                   
-               //    const userData = await userDataRaw.json()
+//                //    const userData = await userDataRaw.json()
 
-               //    console.log('네이버 콜백 함수 결과', userData)
-               //    res.send({ userData });
-               // } else {
-               //    console.log();
+//                //    console.log('네이버 콜백 함수 결과', userData)
+//                //    res.send({ userData });
+//                // } else {
+//                //    console.log();
                   
-               //    res.send({ result })
-               // }
-            } catch (err) {
-               console.error(err);
-               return next(err);
-            }
-         }
-   )(req, res, next)
+//                //    res.send({ result })
+//                // }
+//             } catch (err) {
+//                console.error(err);
+//                return next(err);
+//             }
+//          }
+//    )(req, res, next)
+// });
+
+authRouter.get("/naver/callback", isNotLoggedIn, async (req, res) => {
+   // 토큰을 발급받으려면 query string으로 넘겨야 할 정보들이다.
+   const code = req.query.code
+   const state = 'c5db2fc8-f965-4789-9197-857ce81c60f6';
+
+   const clientId = process.env.NAVER_ID as string
+   const clientSecret = process.env.NAVER_SECRET as string
+   const redirectURI = encodeURI('http://localhost:8000/auth/naver/callback')
+
+    // 로그인 API를 사용해 access token을 발급받는다.
+   const naver_api_url = `https://nid.naver.com/oauth2.0/token?grant_type=authorization_code&response_type=code&client_id=${clientId}&client_secret=${clientSecret}&redirect_uri=${redirectURI}&code=${code}&state=${state}`;
+   console.log('naver_api_url::: ', naver_api_url);
+   
+   const options = {
+       url: naver_api_url,
+       headers: {
+         'X-Naver-Client-Id': clientId, 
+         'X-Naver-Client-Secret': clientSecret
+       }
+   }
+   const result = await request.get(options);
+    // string 형태로 값이 담기니 JSON 형식으로 parse를 해줘야 한다.
+   const token = JSON.parse(result).access_token;
+
+   console.log('token:::', token);
+   
+ 
+   // 발급 받은 access token을 사용해 회원 정보 조회 API를 사용한다.
+   const info_options = {
+       url: 'https://openapi.naver.com/v1/nid/me',
+       headers: {'Authorization': 'Bearer ' + token}
+   };
+
+   const info_result = await request.get(info_options);
+   console.log('info_result', info_result);
+   
+    // string 형태로 값이 담기니 JSON 형식으로 parse를 해줘야 한다.
+   const info_result_json = JSON.parse(info_result).response;
+
+   res.send({ info_result_json })
 });
 
 // 네이버 회원 프로필 조회 API
