1d1a5e40cb7bccc030219a4dd3a9209bdbdfa069
강유신 <rkddbtls007@naver.com>
2023-10-09 22:36:48 +0900

    feat: DB연결 포트 설정

--- shared/lib/db.ts
+++ shared/lib/db.ts
@@ -16,6 +16,7 @@ export const connection = mysql.createConnection({
 	user: process.env.MYSQL_USER,
 	password: process.env.MYSQL_PASSWORD,
 	database: process.env.MYSQL_DATABASE,
+	port: Number(process.env.MYSQL_PORT) || 3306,
 })
 
 export class AppleFarmDBClient {
