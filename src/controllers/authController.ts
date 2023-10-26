efe3be43e11dd5bb4bbeaf4824c807431b6fceb8
강유신 <rkddbtls007@naver.com>
2023-10-26 16:05:45 +0900

    fix: 502 에러 수정 중

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,20 +1,20 @@
-// import express from 'express'
-// import { AuthService } from '../services/authService'
-// import { CustomExpressRequest } from '../../shared/lib/expressRequest'
-// import { authService } from '../services'
+import express from 'express'
+import { AuthService } from '../services/authService'
+import { CustomExpressRequest } from '../../shared/lib/expressRequest'
+import { authService } from '../services'
 
-// export class AuthController {
-// 	private authService: AuthService
+export class AuthController {
+	private authService: AuthService
 
-// 	constructor(authService: AuthService) {
-// 		this.authService = authService
-// 	}
+	constructor(authService: AuthService) {
+		this.authService = authService
+	}
 
-// 	public async login(req: CustomExpressRequest, res: express.Response) {
-// 		const { email, password } = req.body
+	public async login(req: CustomExpressRequest, res: express.Response) {
+		const { email, password } = req.body
 
-// 		const result = await authService.loginService({ email, password })
+		const result = await authService.loginService({ email, password })
 
-// 		return res.status(200).json(result)
-// 	}
-// }
+		return res.status(200).json(result)
+	}
+}
--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,21 +1,15 @@
 import { UserController } from './userController'
 import { PostController } from './postController'
-import { userService, postService, orderService } from '../services'
-// import { AuthController } from './authController'
+import { userService, postService, orderService, authService } from '../services'
+import { AuthController } from './authController'
 import { OrderController } from './orderController'
 import { userRepository } from '../repositorys'
 import { ProductController } from './productController'
 
 const userController = new UserController(userService)
 const postController = new PostController(postService, userService)
-// const authController = new AuthController()
+const authController = new AuthController(authService)
 const orderController = new OrderController(userRepository, orderService)
 const productController = new ProductController(userRepository)
 
-export {
-	userController,
-	postController,
-	orderController,
-	productController,
-	// authController
-}
+export { userController, postController, authController, orderController, productController }
