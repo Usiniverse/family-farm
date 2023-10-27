1b4805cb0544dabee105f29aa62af3e98f8b7497
강유신 <rkddbtls007@naver.com>
2023-10-28 01:35:35 +0900

    feat: 기존회원 확인

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -6,6 +6,7 @@ import { GetUserDTO } from '../dtos/users/getUserDTO'
 import { userService } from '../services'
 import { CustomExpressRequest } from '../../shared/lib/expressRequest'
 import { UpdateUserDTO } from '../dtos/users/updateUserDTO'
+import { userRepository } from '../repositorys'
 
 export class UserController {
 	private userService: UserService
@@ -14,13 +15,12 @@ export class UserController {
 		this.userService = userService
 	}
 
-	public async createUserController(
-		req: CustomExpressRequest,
-		res: Response,
-	): Promise<UserDTO | any> {
+	public async createUserController(req: CustomExpressRequest, res: Response) {
 		const dto: CreateUserDTO = req.body as CreateUserDTO
 
-		if (dto.email === req.body.email) {
+		const user = await userRepository.getUser(dto.email)
+
+		if (user.email === req.body.email) {
 			return res.status(200).json({ message: '이미 가입한 회원입니다.' })
 		}
 
@@ -29,25 +29,23 @@ export class UserController {
 		return res.status(200).json(result)
 	}
 
-	public async getUserByEmail(req: CustomExpressRequest, res: Response): Promise<UserDTO> {
+	public async getUserByEmail(req: CustomExpressRequest, res: Response) {
 		const { email }: GetUserDTO = req.body
 
 		const result = await userService.getUserByEmail({ email })
 
-		res.json(result)
-		return result
+		return res.status(200).send(result)
 	}
 
-	public async getUserBySnsId(req: CustomExpressRequest, res: Response): Promise<UserDTO> {
+	public async getUserBySnsId(req: CustomExpressRequest, res: Response) {
 		const { sns_id }: GetUserDTO = req.body as GetUserDTO
 
 		const result = await userService.getUserBySnsId({ sns_id })
 
-		res.json(result)
-		return result
+		return res.status(200).send(result)
 	}
 
-	public async updateUser(req: CustomExpressRequest, res: Response): Promise<UserDTO> {
+	public async updateUser(req: CustomExpressRequest, res: Response) {
 		const id = req.auth.id
 		// const id = req.auth.id
 
@@ -55,18 +53,16 @@ export class UserController {
 
 		const result = await userService.updateUser(dto)
 
-		res.json(result)
-		return result
+		return res.status(200).send(result)
 	}
 
-	public async deleteUser(req: CustomExpressRequest, res: Response): Promise<void> {
+	public async deleteUser(req: CustomExpressRequest, res: Response) {
 		const id = req.auth.id
 
 		await userService.getUserById(id)
 
 		const result = await userService.deleteUser(id)
 
-		res.json(result)
-		return result
+		return res.status(200).send(result)
 	}
 }
