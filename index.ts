eacb3f3522e8233034f986cf45e66a8c896be3a5
강유신 <rkddbtls007@naver.com>
2024-02-16 15:36:22 +0900

    fix: 개발환경 cors에러

--- index.ts
+++ index.ts
@@ -28,7 +28,8 @@ const appServer = async () => {
 	app.use(cors(corsOptions))
 
 	app.use(function (req, res, next) {
-		res.setHeader('Access-Control-Allow-Origin', ['https://apple-fe-test.vercel.app', '*'])
+		res.setHeader('Access-Control-Allow-Origin', '*')
+		// res.setHeader('Access-Control-Allow-Origin', 'https://apple-fe-test.vercel.app')
 		res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
 		res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
 		next()
